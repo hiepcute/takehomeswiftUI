0565d22be500523b1430bfea8fc860a8ae3ade46
do hiep <hiepzxc16@gmail.com>
2025-02-15 13:33:42 +0700

    Initial Commit

---
+++ TakeHomeSwiftUI/TakeHomeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeSwiftUIApp.swift
+//  TakeHomeSwiftUI
+//
+//  Created by MACBOOK on 15/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
