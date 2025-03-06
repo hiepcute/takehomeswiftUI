afb471fbad7e93c5bfa8a08b24779b780e82dfe1
do hiep <hiepzxc16@gmail.com>
2025-03-06 22:10:47 +0700

    Push all files

--- TakeHomeSwiftUI/ContentView.swift
+++ TakeHomeSwiftUI/ContentView.swift
@@ -9,16 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                headerView
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension ContentView {
+    var headerView: some View {
+        HStack {
+            Image(systemName: "arrow.backward")
+                .font(.headline)
+                .foregroundColor(.black)
+                .frame(width: 50, height: 50)
+            Spacer()
+            Text("List View")
+            Spacer()
+            Image(systemName: "arrow.backward")
+                .font(.headline)
+                .foregroundColor(.black)
+                .frame(width: 50, height: 50)
+                .rotationEffect(Angle(degrees: 180))
+            
+            
+           
+        }
+    }
 }
